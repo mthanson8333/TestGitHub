198354dfd92fc5b1fb157ea82affc85de05e4382
mthanson8333 <mthanson8333@gmail.com>
2024-01-12 21:22:09 -0500

    Add project files.

---
+++ TestGitHub/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestGitHub
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestGitHub/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestGitHub
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
